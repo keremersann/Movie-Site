1a5c845bfc5ecee00e4a3362fac2a99f5f040797
keremersann <91527036+keremersann@users.noreply.github.com>
2022-05-19 05:14:13 +0300

    Update App.js
    Main component is added to the App.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar'
+import Main from "./Components/Main"
 function App() {
   return (
     <div>
       <Navbar/>
+      <Main />
     </div>
   )
 }
