8f510a560e4001aee17239905df1cfec57a7d46c
keremersann <91527036+keremersann@users.noreply.github.com>
2022-05-19 04:35:40 +0300

    navbar design is completed. logo is changed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar()
 {
     return(
         <nav className = "navbar">
-            <img src = "./images/series-logo.jpg" alt = "series logo" className = "navbar-logo-image"></img>
+            <img src = "./images/series-logo.webp" alt = "series logo" className = "navbar-logo-image"></img>
             <div className = "navbar-content">
                 <h4 className = "navbar-title">CS391 Final Project - Movie Website</h4>
                 <ul className = "navbar-unordered-list">
@@ -14,7 +14,6 @@ function Navbar()
                     <li>What we do</li>
                     <li>Contact us</li>
                 </ul>
-                
             </div>
             <button className = "navbar-add-movie-button">Add Movie</button>
         </nav>
