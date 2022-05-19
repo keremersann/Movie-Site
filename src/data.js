e38ba04416d375857bdc06b6637e88101ef76763
keremersann <91527036+keremersann@users.noreply.github.com>
2022-05-19 05:01:52 +0300

    Create data.js
    geçiçi data.js dosyası oluşturuldu. API dan çekeceğiz bu bilgileri ileride.

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        "name": "Pulp Fiction",
+        "imdb": "8.9",
+        "category": ["black comedy","crime"],
+        "director": "Quentin Tarantino"
+    },
+    {
+        "name": "The Godfather",
+        "imdb": "9.2",
+        "category": ["crime","drama"],
+        "director": "Francis Ford Coppola"
+    },
+    {
+        "name": "Unforgiven",
+        "imdb": "8.2",
+        "category": ["drama","western"],
+        "director": "Clint Eastwood"
+    },
+    {
+        "name": "The Pianist",
+        "imdb": "8.5",
+        "category": ["music","drama","biography"],
+        "director": "Roman Polanski"
+    },
+    {
+        "name": "Mulholland Drive",
+        "imdb": "7.9",
+        "category": ["drama","mystery","thriller"],
+        "director": "David Lynch"
+    }
+]
