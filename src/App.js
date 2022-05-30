ccf57f9385d7f3f1f1a8c4a8a9729a3fd4592b24
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2022-05-31 02:17:56 +0300

    Navbar gain responsive view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar'
-import {Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="row">
       <Navbar/>
       <Outlet/>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,38 @@
 import "../Css/Navbar.css"
 import React from 'react'
 import {Link} from "react-router-dom"
+import {Row, Col} from 'react-bootstrap';
 
 
-function Navbar()
-{
+export default function Navbar(){
     return(
-        <nav className = "navbar">
-            <img src = "./images/series-logo.webp" alt = "series logo" className = "navbar-logo-image"></img>
-            <div className = "navbar-content">
+        <Row className="navbar">
+            <Col xs={6} md={6} lg={2}>
+                <img src = "./images/series-logo.webp" alt = "series logo" className = "navbar-logo-image"></img>
+            </Col>
+            <Col xs={6} md={6} lg={2}>
                 <h4 className = "navbar-title">CS391 Final Project - Movie Website</h4>
+            </Col>
+            <Col xs={6} md={2} lg={2}>
                 <Link to="/">
-                <button className = "navbar-button">Main Page</button> 
+                    <button className = "navbar-button">Main Page</button> 
                 </Link>
+            </Col>
+            <Col xs={6} md={2} lg={2}>
                 <Link to="/Team">
-                <button className = "navbar-button">Our Team</button> 
+                    <button className = "navbar-button">Our Team</button> 
                 </Link>
-                <Link to="/Team">
-                <button className = "navbar-button">About Project</button> 
+            </Col>
+            <Col xs={6} md={2} lg={2}>
+                <Link to="/About">
+                    <button className = "navbar-button">About Project</button> 
                 </Link>
+            </Col>
+            <Col xs={6} md={2} lg={2}>
                 <Link to="/AddMovie">
-                <button className = "navbar-button">Add Movie</button> 
+                    <button className = "navbar-button">Add Movie</button> 
                 </Link>
-            </div>
-        </nav>
+            </Col>
+        </Row>
     )
 }
-
-export default Navbar
