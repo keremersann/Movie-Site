eb5a79be8c9ac25327c1de485c47424401657082
KaanAlp01 <kaan.alp@ozu.edu.tr>
2022-06-01 05:07:21 +0300

    Fıt the team cards in the teams page

--- src/Containers/Team.js
+++ src/Containers/Team.js
@@ -1,10 +1,20 @@
+import "../Css/Card.css"
+import TeamCard from "../Components/TeamCard"
+import "../Css/Main.css"
+
+
+
 export default function Team() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Team</h2>
-        <p>Kerem Ersan:</p>
-        <p>Musa Berkay:</p>
-        <p>Kaan Alp:</p>
+        <h2>THE TEAM</h2>
+        <div className = "main">
+                <div className = "grid--view">
+                <TeamCard title ={"Kerem Ersan"} description ={item.description} age ={"22"} occupation ={"Computer Science"} favouriteMovie ={"Resevoir Dogs"}></TeamCard>
+                <TeamCard title ={"Musa Berkay"} description ={item.description} age ={"22"} occupation ={"Computer Science"} favouriteMovie ={"Baywatch"}></TeamCard>
+                <TeamCard title ={"Kaan Alp"} description ={item.description} age ={"22"} occupation ={"Computer Science"} favouriteMovie ={"Nice Guys"}></TeamCard>
+                </div>
+            </div>
       </main>
     );
   }
