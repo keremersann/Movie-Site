aa1b2966b043bd44ab153a0da0dd040f1f412913
KaanAlp01 <kaan.alp@ozu.edu.tr>
2022-06-01 05:08:04 +0300

    Added a "user manual" style functinality to about

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -1,8 +1,20 @@
+import "../Css/Main.css"
+import React, {useState, useEffect} from 'react'
+import MovieCard from "../Components/MovieCard"
+
 export default function About() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>About</h2>
         <p>This website is designed for the user to easily access their movie lists.</p>
+        <p>This site works as follows:</p>
+        <p>You can add and delete any movie that you can think about to this website.</p>
+        <p>Then you can see your list in -INSERT PAGE NAME HERE-</p>
+        <p>These cards will help keep track of your movies and it contains information about your movies such as the director and their IMDB rating.</p>
+        <div>
+          MovieCard title ={"TITLE"} description ={"DESCRIPTION"} rating ={"RATING"} language ={"LANGUAGE"} director ={"Director"}
+        </div>
+        <h6>HAVE FUN AND CREATE GOOD LISTS</h6>
       </main>
     );
   }
