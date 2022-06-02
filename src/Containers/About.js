f2a633be82cb37689d107373b76db026bc415920
KaanAlp01 <kaan.alp@ozu.edu.tr>
2022-06-02 14:10:02 +0300

    About messages changed

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -7,16 +7,16 @@ export default function About() {
       <main style={{ padding: "1rem 0" }}>
         <h2>About</h2>
         <Container className = "form-parent-container">
-        <p>This website is designed for the user to easily access their movie lists.</p>
-        <p>This site works as follows:</p>
+        <p>This website is designed for you to easily access your movie list.</p>
+        <p>Here is how you can use it:</p>
         <p>- You can add and delete any movie that you can think about to this website.</p>
-        <p>- Then you can see your list in -INSERT PAGE NAME HERE-</p>
-        <p>- These cards will help keep track of your movies and it contains information about your movies such as the director and their IMDB rating.</p>
+        <p>- Then you can see your list in the main page from the cards.</p>
+        <p>- These cards will help you to keep track of your movies and it contains information about your movies such as the director and their IMDB rating.</p>
+        <h6>HAVE FUN AND CREATE GOOD LISTS</h6>
         <div className="grid--view--about">
           MovieCard title ={"TITLE"} description ={"DESCRIPTION"} rating ={"RATING"} language ={"LANGUAGE"} director ={"Director"}
         </div>
         </Container>
-        <h6>HAVE FUN AND CREATE GOOD LISTS</h6>
       </main> 
     );
   }
